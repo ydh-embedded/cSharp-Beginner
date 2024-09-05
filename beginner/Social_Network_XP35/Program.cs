b0a7d4fc688079f04ef32970ab1d8d897f65977d
ydh-embedded <127036806+ydh-embedded@users.noreply.github.com>
2024-09-05 12:52:21 +0200

    add

--- beginner/Social_Network_XP35/Program.cs
+++ beginner/Social_Network_XP35/Program.cs
@@ -1,9 +1,49 @@
-﻿namespace Social_Network_XP35;
+﻿
+using System;
+using System.Collections.Generic;
 
-class Program
+namespace Social_Network_XP35
 {
-    static void Main(string[] args)
+    class Program
     {
-        Console.WriteLine("Hello, World!");
+        static void Main(string[] args)
+        {
+            string? sPostText = Console.ReadLine();
+
+            if (sPostText is not null)
+            {
+                Post post = new Post(sPostText);
+                post.ShowPost();
+            }
+            else
+            {
+                Console.WriteLine("No input provided.");
+            }
+        }
+    }
+
+    class Post
+    {
+        private string text;
+
+        //NOTE - Constructor mit Null-Wert Prüfung
+        public Post(string text)
+        {
+            this.text = text ?? throw new ArgumentNullException(nameof(text));
+        }
+
+        //NOTE - Property for text
+        public string Text 
+        { 
+            get { return text; } 
+            set { text = value; } 
+        }
+
+        //NOTE - changed this to use the property instead of the private field
+        public void ShowPost()
+        {
+            Console.WriteLine("New post");
+            Console.WriteLine(Text); 
+        }
     }
 }
