2d382aa8748dd07d8a9900c06ab21a554ede7165
ydh-embedded <127036806+ydh-embedded@users.noreply.github.com>
2024-09-05 13:29:58 +0200

    dev arrays

---
+++ beginner/Arrays/Program.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace SoloLearn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[ ] b = {11, 45, 62, 70, 88};
+
+            Console.WriteLine(b[2]);
+            Console.WriteLine(b[3]);
+        }
+    }
+}
