6e8868d9637a9e4a780ca5b10edeb1d4f173103a
ydh-embedded <127036806+ydh-embedded@users.noreply.github.com>
2024-09-05 13:30:04 +0200

    dev Loop

---
+++ beginner/Arrays_in_Loops/Program.cs
@@ -0,0 +1,23 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays_in_Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[ ] a = new int[10];
+            for (int k = 0; k < 10; k++) {
+                a[k] = k*2;
+            }
+            for (int k = 0; k < 10; k++) {
+                Console.WriteLine(a[k]);
+            }
+        }
+    }
+}
