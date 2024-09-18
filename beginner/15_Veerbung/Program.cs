13f3f740760802b4c48423e08d0084989b3a7b43
ydh-embedded <127036806+ydh-embedded@users.noreply.github.com>
2024-09-18 13:12:01 +0200

    dev

--- beginner/15_Veerbung/Program.cs
+++ beginner/15_Veerbung/Program.cs
@@ -1,7 +1,54 @@
-﻿namespace _15_Veerbung;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _15_Veerbung;
+class Dog : Animal
+{
+  public Dog() {
+    Legs = 4;
+  }
+  public void Bark() {
+    Console.WriteLine("Woof ");
+  }
+}
+class Person
+{
+  public void Speak()
+  {
+    Console.WriteLine("Hi there from Method Speak in class Person");
+  }
+}
+class Student : Person
+{
+    int number;
+}
+class Teacher : Person
+{
+    public void GradeAssignment()
+    {
+        System.Console.WriteLine("Grading Assignment... from Sub-Class Teacher ");
+    }
+}
+class Professor : Teacher
+{
+    public void PublishResearch()
+    {
+        System.Console.WriteLine("Publishing research paper... from Sub-Class Professor");
+    }
+}
+class Animal
+{
+  public int Legs {get; set;}
+  public int Age {get; set;}
+}
+
 class Program
 {
     static void Main(string[] args)
+
     {
         Console.WriteLine(@"
 
@@ -48,51 +95,69 @@ class Program
 
             **************************************************************************
 
-                    Veerbung
+                Veerbung Inheritance
 
-                    In a ballroom dancing competition,
-                    each dancer from a pair is evaluated separately,
-                    and then their points are summed up to get the total pair score.
-                    The program you are given takes the names and
-                    the points of each dancer as input and creates a DancerPoints
-                    objects for each dancer,
-                    using the taken name and score values as parameters for constructors.
-                    Complete the given class, using overload + operator
-                    to return an new object where the names of
-                    dancers are in one string (see sample output) and
-                    the score is equal to the sum of their points.
-                    The declaration of that object and the output of
-                    its points are already written in Main().
+                Inheritance allows us to define a class based on another class.
+                This makes creating and maintaining an application easy.
 
-                    Definition:
+                The class whose properties are inherited by another class
+                is called the Base class. The class which inherits the properties
+                is called the Derived class.
 
-                    The DancerPoints class has two properties:
-                    name and points, which are set through the constructor.
-
-                    The + operator is overloaded
-                    to take two DancerPoints objects as parameters.
-                    It combines the names of the two dancers with an &
-                    separator and adds their points together.
-                    The resulting DancerPoints object is returned.
+                For example, base class Animal can be used to derive Cat and Dog classes.
+                The derived class inherits all the features from the base class,
+                and can have its own additional features. 
 
 
 
             **************************************************************************");
-            //REVIEW -   (player_1 = Dave points_1 = 8 , player_2 = Jessica | points_2 = 7)
+            //REVIEW -   (  )
 
             //REVIEW - Input
-            Console.Write("Enter the name of the first dancer: ");
-            string sName_1 = Console.ReadLine();
-            Console.Write("Enter the points of the first dancer: ");
-            int  iPoints_1 = Convert.ToInt32(Console.ReadLine());
+            //STUB - Console.Write("Enter the name of the first dancer: ");
+            //STUB - string sName_1 = Console.ReadLine();
+            //STUB - Console.Write("Enter the points of the first dancer: ");
+            //STUB - int  iPoints_1 = Convert.ToInt32(Console.ReadLine());
+            //STUB - 
+            //STUB - Console.Write("Enter the name of the second dancer: ");
+            //STUB - string sName_2 = Console.ReadLine();
+            //STUB - Console.Write("Enter the points of the second dancer: ");
+            //STUB - int  iPoints_2 = Convert.ToInt32(Console.ReadLine());
 
-            Console.Write("Enter the name of the second dancer: ");
-            string sName_2 = Console.ReadLine();
-            Console.Write("Enter the points of the second dancer: ");
-            int  iPoints_2 = Convert.ToInt32(Console.ReadLine());
+            //REVIEW - Wir haben ein Dog Objekt erstellt und die Bark Methode aufgerufen,
+            //         die in der Basisklasse Dog deklariert wurde.  (  )
+            Dog d = new Dog();
+            Console.Write("A Dog has " + d.Legs + " Legs " + " (Inheritance from class Dog) ");
+            
+            
+            d.Bark();
 
+            //REVIEW - Wir haben ein Student Objekt erstellt und die Speak Methode aufgerufen,
+            //         die in der Basisklasse Person deklariert wurde.  (  )
+            Person person = new Person();
+            person.Speak();                             // Output: Hi there
+
+            //REVIEW - Wir haben ein Student Objekt erstellt und die Speak Methode aufgerufen,
+            //         die in der Basisklasse Person deklariert wurde.  (  )
+            Student student = new Student();
+            student.Speak();                            // Output: Hi there
+
+            //REVIEW - Wir haben ein Student Objekt erstellt und die Speak Methode aufgerufen,
+            //         die in der Basisklasse Person deklariert wurde.  (  )
+            Teacher teacher = new Teacher();
+            teacher.Speak();                            // Output: Hi there
+            teacher.GradeAssignment();                  // Output: Grading assignment...
+
+            //REVIEW - Wir haben ein Student Objekt erstellt und die Speak Methode aufgerufen,
+            //         die in der Basisklasse Person deklariert wurde.  (  )
+            Professor professor = new Professor();
+            professor.Speak();                          // Output: Hi there
+            professor.GradeAssignment();                // Output: Grading assignment...
+            professor.PublishResearch();                // Output: Publishing research paper...
+
+            
 
     }
+
+    
 }
-
-
