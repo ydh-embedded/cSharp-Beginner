91878647d45975d9ef558e784e2f6d3082803f7a
ydh-embedded <127036806+ydh-embedded@users.noreply.github.com>
2024-09-23 12:22:27 +0200

    dev Interface

---
+++ beginner/19_0_Interfaces/Circle.cs
@@ -0,0 +1,11 @@
+namespace _19_0_Interfaces;
+
+//REVIEW - inheritate class Circle from Shape
+
+public class Circle : I_Shape
+{
+    public void Draw()
+    {
+        System.Console.WriteLine("Circle Draw");
+    }
+}
---
+++ beginner/19_0_Interfaces/I_Shape.cs
@@ -0,0 +1,9 @@
+namespace _19_0_Interfaces;
+
+//REVIEW - Interface Shape
+
+public interface I_Shape
+{
+
+    abstractvoid Draw();        //SECTION - Interfaces sind völlig abstracte Klassen und enthalten nur abstrakte Mitglieder
+}
