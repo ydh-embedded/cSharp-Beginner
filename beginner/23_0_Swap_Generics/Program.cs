5e1904a4f076894f01aa8f6c26834edcc396d0fc
ydh-embedded <127036806+ydh-embedded@users.noreply.github.com>
2024-09-25 15:27:06 +0200

    dev

--- beginner/23_0_Swap_Generics/Program.cs
+++ beginner/23_0_Swap_Generics/Program.cs
@@ -1,12 +1,10 @@
 ﻿namespace _23_0_Swap_Generics;
 
+//REVIEW - class Program
 class Program
 {
-//REVIEW - class Program
     static void Main(string[] args)
     {
-
-
         Console.WriteLine(@"
 
             **************************************************************************
@@ -62,44 +60,92 @@ class Program
                 für die Verwendung generischer Methoden in C#.
 
             **************************************************************************");
-                
+
+        Console.WriteLine(@"
+
+            **************************************************************************
+        
+        Übersicht
+
+        Dieser Code demonstriert eine generische Swap-Funktion in C#.
+        Die Klasse Utilities enthält eine statische Methode Swap,
+        die die Werte von zwei Variablen desselben Typs vertauscht.
+        Der Code verwendet diese Methode,
+        um die Werte von zwei int-Variablen und zwei String-Variablen zu vertauschen.
+
+        
+        Hauptmethode
+
+        Die Hauptmethode initialisiert zwei int-Variablen
+        
+             a und b
+
+        mit den Werten
+
+             4 bzw. 9.
+        
+        Dann ruft die Swap-Methode diese der Klasse Utilities auf und
+        übergibt a und b als Referenz mit dem Schlüsselwort ref.
+        Nach dem Tausch gibt sie die Werte von a und b auf der Konsole aus.
+
+        Der gleiche Vorgang wird für zwei String-Variablen 
             
+            x und y
+        
+        mit den Werten „Hello“ bzw. „World“ wiederholt.
 
-    //REVIEW - class Utilities
-    static class Utilities
-    {
-        public static void Swap<T>(ref T a, ref T b)
-        {
-            T temp = a;
-            a = b;
-            b = temp;
-        }
-    }
+        
+        Utilities-Klasse und Swap-Methode
+
+        Die Klasse Utilities ist eine statische Klasse,
+        die eine einzige statische Methode Swap enthält.
+        Diese Methode ist generisch, das heißt,
+        sie kann mit jedem Typ T arbeiten.
+
+        Die Swap-Methode nimmt zwei Parameter
+        
+             a und b [vom Typ T]
+             
+        mit Hilfe des Schlüsselworts 
+        
+                ref
+        
+        auf. Anschließend werden die Werte von a und b
+        mit Hilfe einer temporären Variablen temp vertauscht.
 
 
+        Hier eine schrittweise Erklärung des Swap-Prozesses:
 
+        T temp = a;   - Erstellen Sie eine temporäre Variable temp und
+                        weisen Sie ihr den Wert von a zu.
 
-            int a = 4, b = 9;
-            Swap<int>(ref a, ref b);
+        a = b;        - Weisen Sie a den Wert von b zu.
+        b = temp;     - Weisen Sie b den Wert von temp zu
+                        (das ist der ursprüngliche Wert von a).
+
+        Auf diese Weise werden die Werte von a und b effektiv vertauscht.     
+
+            **************************************************************************
+            ");
+
+        int a = 4, b = 9;
+            Utilities.Swap<int>(ref a, ref b);
             Console.WriteLine(a+" "+b);
             
-            string x = "Hello";
-            string y = "World";
-
-
-            Swap<string>(ref x, ref y);
-            Console.WriteLine(x+" "+y);
-    
+        string x = "Hello";
+        string y = "World";
+            Utilities.Swap<string>(ref x, ref y);
+            Console.WriteLine(x+" "+y);     
     }
 }
-
-//REVIEW - Swap
-class Swap<T>
-{
-    static void Swap<T>(ref T a, ref T b)
-    {
-                T temp = a;
-                a = b;
-                b = temp;
-    }
-}
+//REVIEW - class Utilities
+        static class Utilities
+        {
+            //REVIEW - Swap
+            public static void Swap<T>(ref T a, ref T b)
+            {
+                T temp =    a;
+                a      =    b;
+                b      = temp;
+            }
+        }
