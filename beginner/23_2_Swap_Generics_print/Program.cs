2c8df647ca4e847a06df02d3068ac534d807c3b5
ydh-embedded <127036806+ydh-embedded@users.noreply.github.com>
2024-09-25 15:50:18 +0200

    dev

--- beginner/23_2_Swap_Generics_print/Program.cs
+++ beginner/23_2_Swap_Generics_print/Program.cs
@@ -1,9 +1,71 @@
-﻿namespace _23_2_Swap_Generics_print;
+﻿using System;
+using System.Collections.Generic;
 
+namespace _23_2_Swap_Generics_print;
+
+//REVIEW - class Program
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine(@"
+        
+        Generische Methoden 
+
+        Wir schreiben ein Programm,
+        das den Wert einer Variable beliebigen Typs ausgeben kann.
+        Es nimmt einen String, einen Integer und einen Double-Wert
+        als Eingabe und sollte sie dann ausgeben.
+
+        Erstellen Sie eine generische Methode Print
+        für eine Printer-Klasse, um die gegebenen Aufrufe korrekt auszuführen.
+
+        Beispieleingabe
+
+            Hello
+            14
+            7.6
+
+        Beispielausgabe
+
+            Zeigt Hello
+            Zeigt 14
+            Zeigt 7.6
+        
+        Info:
+        Beachten Sie, dass es kein Objekt der Printer-Klasse gibt,
+        daher sollte die generische Methode statisch sein.
+
+        ");
+
+            //SECTION - Variables declaration         
+            string    text = Console.ReadLine();
+            int     intNum = Convert.ToInt32(Console.ReadLine());
+            double doubNum = Convert.ToDouble(Console.ReadLine());
+
+            //SECTION - Printer
+            Printer.Print(text);
+            Printer.Print(intNum);
+            Printer.Print(doubNum);
+    
+
+
+
+    }
+}
+
+        System.Console.WriteLine(@"
+        
+            
+
+        ");
+
+
+//REVIEW - class Printer
+class Printer
+{
+    public static void Print<T>( T value)       //SECTION - wir erstellen eine statische Methode um diese aufrufen zu können ohne eine Instanz der Printer klasse erstellen zu müssen
+    {                                           //SECTION - Die Print Methode kann jeden Typ annehmen  und ist damit nicht auf die Daten der Instanz der Printer Klasse angewiesen
+        System.Console.WriteLine(value);
     }
 }
