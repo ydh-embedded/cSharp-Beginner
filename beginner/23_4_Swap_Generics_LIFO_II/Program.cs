95159d114c5b0587fea3dd66ff62c39e86be747a
ydh-embedded <127036806+ydh-embedded@users.noreply.github.com>
2024-09-27 10:00:00 +0200

    dev

--- beginner/23_4_Swap_Generics_LIFO_II/Program.cs
+++ beginner/23_4_Swap_Generics_LIFO_II/Program.cs
@@ -1,10 +1,11 @@
-﻿namespace _23_3_Swap_Generics_LIFO_II;
-//REVIEW - class Program
+﻿namespace _23_4_Swap_Generics_LIFO_II;
+
+// REVIEW - class Program
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine(@"
+                Console.WriteLine(@"
 
             @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@%%###%%%@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
             @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@%##*******##%@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
@@ -67,86 +68,97 @@ class Program
                 
 
             **************************************************************************");
-    
-        
-        
-        //SECTION - Object intStack
-        // Stack<int> intStack = new Stack<int>();
 
+            
+        // SECTION - string Object elemn1 (stack with iElements of Names)
+        Elems<string> sElems_1 = new Elems<string>(3);
+        sElems_1.Add("John", "Tamara", "David");
 
-            //SECTION - string Object elemn1 (stack with iElements of Names)
-            Elems<string> sElems_1 = new Elems<string>();
-            sElems_1.Add("John", "Tamara", "David");
+        sElems_1.Show();
 
-            sElems_1.Show();
+        Console.WriteLine();
 
-            Console.WriteLine();
+        // SECTION - string Object elemn2 (stack with iElements of Numbers)
+        Elems<int> sElems_2 = new Elems<int>(3);
+        sElems_2.Add(5, 14, 13);
 
-            //SECTION - string Object elemn2 (stack with iElements of Numbers)
-            Elems<int> sElems_2 = new Elems<int>();
-            sElems_2.Add(5, 14, 13);
-
-            sElems_2.Show();
-
-    
+        sElems_2.Show();
     }
 }
 
-//REVIEW - class Stack
+// REVIEW - class Stack
 public class Stack<T>
 {
-    //SECTION - var Initialisierung
-    private T[] innerArray = new T[100] ;
-    private int index = 0;
+    protected T[] innerArray;
+    public int index;
 
+    public Stack(int size)
+    {
+        innerArray = new T[size];
+        index = 0;
+    }
 
-    //SECTION - Push to stack
-    public void Push (T item) {innerArray[index++] = item;}
+    public void Push(T item)
+    {
+        if (index < innerArray.Length)
+        {
+            innerArray[index++] = item;
+        }
+        else
+        {
+            throw new InvalidOperationException("Stack is full");
+        }
+    }
 
-    //SECTION - PoP from stack 
-    public T   Pop ( ) {return innerArray[--index] ;}
+    public T Pop()
+    {
+        if (index > 0)
+        {
+            return innerArray[--index];
+        }
+        else
+        {
+            throw new InvalidOperationException("Stack is empty");
+        }
+    }
 
-    //SECTION - Get from stack
-    public T Get (int k ) {return innerArray[k];}
+    public T Get(int k)
+    {
+        if (k < index)
+        {
+            return innerArray[k];
+        }
+        else
+        {
+            throw new InvalidOperationException("Index out of range");
+        }
+    }
 }
 
-
-
-//REVIEW - class Elems
-public class Elems <T>      //SECTION - In this specific case, the constructor public Elems(int size) is used to initialize the Elems object with a specified size, which is then used to create an array of type T with that size.
+// REVIEW - class Elems
+public class Elems<T>
 {
-    public int[] iElements = new int[3];
+    public Stack<T> stack;
 
-    private T[] iElements;
-
-    //SECTION - Constructor Elemns  Constructors are used to set the initial state of an object when it is created
     public Elems(int size)
     {
-        iElements = new T[size];
+        stack = new Stack<T>(size);
     }
 
-
-
-    //SECTION - Method Add 
     public void Add(params T[] elements)
     {
-        for (int i = 0; i < elements.Length; i++)
+        foreach (T element in elements)
         {
-            iElements[i] = elements[i];
+            stack.Push(element);
         }
     }
 
-
-
-    //SECTION - Method Show
     public void Show()
     {
-        foreach (T item in iElements)
+        while (stack.index > 0)
         {
-            Console.Write(item + " ");
+            Console.Write(stack.Pop() + " ");
         }
+        Console.WriteLine();
     }
-
-
-
 }
